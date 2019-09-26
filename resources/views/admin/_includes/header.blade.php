2c853029789a600a58e6f9c8d54f08c2d97271d0
fellipebastos <fellipebastoswm@gmail.com>
2019-09-26 20:44:40 -0300

    Remove dropdown de teste

--- resources/views/admin/_includes/header.blade.php
+++ resources/views/admin/_includes/header.blade.php
@@ -27,20 +27,6 @@
       </li>
       @endif
       @else
-      <li class="nav-item">
-        <div class="dropdown">
-          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown link
-          </a>
-
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </div>
-      </li>
       <li class="nav-item dropdown">
         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false" v-pre>
