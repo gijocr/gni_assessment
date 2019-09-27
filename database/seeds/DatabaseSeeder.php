4c421ecbe2a088483f36d0fbcd45faefadd91df9
fellipebastos <fellipebastoswm@gmail.com>
2019-09-26 22:31:45 -0300

    Adiciona seeder de usuario

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ConfigsTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            ConfigsTableSeeder::class,
+        ]);
     }
 }
